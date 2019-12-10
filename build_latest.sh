a5ae44176064ad617516e1ab1dc429a4936a79ad
Huidae Cho <grass4u@gmail.com>
2019-12-10 13:36:21 -0500

    Add build_latest.sh

---
+++ build_latest.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+export MINGW_CHOST=x86_64-w64-mingw32
+export PATH="/mingw64/bin:$PATH"
+. ~/.ssh/agentrc
+
+(
+cd ~/usr/grass/grass
+git checkout master
+git fetch --all
+git merge upstream/master
+git checkout hcho
+git merge master
+make clean
+../myconfigure.sh
+../mymake.sh
+../deploy.sh
+) > ~/usr/grass/update.log 2>&1
