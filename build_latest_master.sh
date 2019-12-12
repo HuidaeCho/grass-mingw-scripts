a6a0f51c6014753b29fcd6912fe0df221edc678f
Huidae Cho <grass4u@gmail.com>
2019-12-12 08:46:56 -0500

    Add build_latest_master.sh

---
+++ build_latest_master.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This script builds the latest version of the master branch of
+# https://github.com/OSGeo/grass.git or
+# https://github.com/HuidaeCho/grass.git.
+
+set -e
+export MINGW_CHOST=x86_64-w64-mingw32
+export PATH="/mingw64/bin:$PATH"
+. ~/.ssh/agentrc
+
+(
+cd ~/usr/grass/grass
+git checkout master
+git fetch --all
+git merge upstream/master
+make clean
+../myconfigure.sh
+../mymake.sh
+../package.sh
+) > ~/usr/grass/update.log 2>&1
