172417e6521b14494c1e5ba9665cf7512b25b965
Huidae Cho <grass4u@gmail.com>
2019-12-18 19:18:18 -0500

    Zip to full name

--- package.sh
+++ package.sh
@@ -8,14 +8,16 @@ OSGEO4W_MSYS_ROOT='/c/OSGeo4W64'
 OPT_PATH=$OSGEO4W_MSYS_ROOT/opt
 GRASS_PATH=$OPT_PATH/grass
 GRASS_VERSION=`sed -n '/^INST_DIR/{s/^INST_DIR.*grass//; p}' include/Make/Platform.make`
-GRASS_ZIP=~/usr/grass/grass$GRASS_VERSION.zip
+ARCH=x86_64-w64-mingw32
+DATE=`date +%Y%m%d`
+GRASS_ZIP=~/usr/grass/grass$GRASS_VERSION-$ARCH-$DATE.zip
 
 test -e $GRASS_PATH && rm -rf $GRASS_PATH
 test -e $OPT_PATH || mkdir -p $OPT_PATH
-cp -a dist.x86_64-w64-mingw32 $GRASS_PATH
+cp -a dist.$ARCH $GRASS_PATH
 rm -f $GRASS_PATH/grass$GRASS_VERSION.tmp
-cp -a bin.x86_64-w64-mingw32/grass$GRASS_VERSION.py $GRASS_PATH/etc
-cp -a `ldd dist.x86_64-w64-mingw32/lib/*.dll | awk '/mingw64/{print $3}' | sort -u | grep -v 'lib\(crypto\|ssl\)'` $GRASS_PATH/lib
+cp -a bin.$ARCH/grass$GRASS_VERSION.py $GRASS_PATH/etc
+cp -a `ldd dist.$ARCH/lib/*.dll | awk '/mingw64/{print $3}' | sort -u | grep -v 'lib\(crypto\|ssl\)'` $GRASS_PATH/lib
 
 (
 sed -e 's/^\(set GISBASE=\).*/\1%OSGEO4W_ROOT%\\opt\\grass/' \
@@ -34,5 +36,5 @@ sed -e 's/^\(call "%~dp0\)\(.*\)$/\1\\..\\..\\bin\2/' \
 
 cd $OSGEO4W_MSYS_ROOT/..
 OSGEO4W_BASENAME=`basename $OSGEO4W_MSYS_ROOT`
-test -e $GRASS_ZIP && rm -f $GRASS_ZIP
+rm -f ~/usr/grass/grass*-$ARCH-*.zip
 zip -r $GRASS_ZIP $OSGEO4W_BASENAME -x "$OSGEO4W_BASENAME/var/*" "*/__pycache__/*"
