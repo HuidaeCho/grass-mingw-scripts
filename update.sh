61b5639c40dcffd26a2a45358d8830625dcbb09f
Huidae Cho <grass4u@gmail.com>
2019-12-20 14:30:23 -0500

    Smart merge...

--- update.sh
+++ update.sh
@@ -8,11 +8,29 @@ export PATH="/mingw64/bin:$PATH"
 
 (
 cd ~/usr/grass/grass
-git checkout master
+branches=`git branch -a --format='%(refname:short)'`
+
 git fetch --all
-git merge upstream/master
-git checkout hcho
-git merge master
+git checkout master
+# if upstream/master exists, assume it's https://github.com/OSGeo/grass.git's
+# master branch
+if echo "$branches" | grep -q '^upstream/master$'; then
+	# merge OSGeo's master
+	git merge upstream/master
+else
+	# merge origin/master (either OSGeo's or HuidaeCho's master)
+	git merge origin/master
+fi
+# if origin/hcho exists, assume it's https://github.com/HuidaeCho/grass.git's
+# hcho branch
+if echo "$branches" | grep -q '^origin/hcho$'; then
+	# use hcho because he's cool ;-)
+	git checkout hcho
+	# merge origin/hcho
+	git merge origin/hcho
+	# merge master already merged with upstream/master or origin/master
+	git merge master
+fi
 ../myconfigure.sh
 ../mymake.sh clean default
 ../mkbats.sh
