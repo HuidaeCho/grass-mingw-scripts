42332ea97d6064f2b23c91befeca1ac27531f01a
Huidae Cho <grass4u@gmail.com>
2019-12-23 20:28:34 -0500

    Remove usr/grass

--- mkbats.sh
+++ mkbats.sh
@@ -6,20 +6,36 @@ set -e
 . ${GRASSBUILDRC-~/.grassbuildrc}
 cd $GRASS_SRC
 
+case $SYSTEM_BIT in
+64)
+	ARCH=x86_64-w64-mingw32
+	;;
+32)
+	ARCH=i686-w64-mingw32
+	;;
+*)
+	echo "$SYSTEM_BIT: unknown system bit"
+	exit 1
+esac
+
 OSGEO4W_ROOT_MSYS=$OSGEO4W
 OSGEO4W_ROOT=`echo $OSGEO4W_ROOT_MSYS | sed 's#^/##; s#/#:\\\\#; s#/#\\\\#g'`
 MSYS2_ROOT=`echo $WD | sed 's#\\\\usr.*##'`
-MINGW_ROOT=`echo "$MSYS2_ROOT$MINGW_PREFIX" | sed 's#/#\\\\#g'`
+MINGW_ROOT=`echo "$MSYS2_ROOT$MINGW_PREFIX" | tr / '\\\\'`
 
-test -e $BATCH_DIR || mkdir $BATCH_DIR
+GRASS_BIN="$GRASS_SRC/bin.$ARCH"
+GRASS_DIST="$GRASS_SRC/dist.$ARCH"
 
-(
-sed -e 's/^\(set GISBASE=\).*/\1%HOME%\\usr\\grass\\grass\\dist.'$MINGW_CHOST'/' \
+GRASS_SRC_WIN=`pwd -W | tr / '\\\\'`
+GRASS_BIN_WIN="$GRASS_SRC_WIN\\bin.$ARCH"
+GRASS_DIST_WIN="$GRASS_SRC_WIN\\dist.$ARCH"
+
+sed -e 's/^\(set GISBASE=\).*/\1'$GRASS_DIST_WIN'/' \
     mswindows/osgeo4w/env.bat.tmpl
 echo
 echo "set PATH=$MINGW_ROOT\\bin;%OSGEO4W_ROOT%\\apps\\msys\\bin;%PATH%"
-) > $BATCH_DIR/env.bat
-unix2dos $BATCH_DIR/env.bat
+) > $GRASS_DIST/etc/env.bat
+unix2dos $GRASS_DIST/etc/env.bat
 
 OSGEO4W_ROOT_ESCAPED=`echo $OSGEO4W_ROOT | sed 's/\\\\/\\\\\\\\/g'`
 MSYS2_ROOT_ESCAPED=`echo $MSYS2_ROOT | sed 's/\\\\/\\\\\\\\/g'`
@@ -32,8 +48,8 @@ HOME_ESCAPED=`echo $HOME_ESCAPED | sed 's#//*#\\\\\\\\#g'`
 VERSION=`sed -n '/^INST_DIR[ \t]*=/{s/^.*grass//; p}' include/Make/Platform.make`
 (
 sed -e 's/^\(call "\)%~dp0\(.*\)$/\1'$OSGEO4W_ROOT_ESCAPED'\\bin\2\nSET HOME='$HOME_ESCAPED'/' \
-    -e 's/^call "%OSGEO4W_ROOT%.*\\env\.bat"$/call "%HOME%\\usr\\grass\\bin\\env.bat"/' \
-    -e 's/^\("%GRASS_PYTHON%" "\).*\?\(".*\)/\1%HOME%\\usr\\grass\\grass\\bin.'$MINGW_CHOST'\\grass'$VERSION'.py\2/' \
+    -e 's/^call "%OSGEO4W_ROOT%.*\\env\.bat"$/call "'$GRASS_DIST_WIN'\\etc\\env.bat"/' \
+    -e 's/^\("%GRASS_PYTHON%" "\).*\?\(".*\)/\1'$GRASS_BIN_WIN'\\grass'$VERSION'.py\2/' \
     mswindows/osgeo4w/grass.bat.tmpl
-) > $BATCH_DIR/grass$VERSION.bat
-unix2dos $BATCH_DIR/grass$VERSION.bat
+) > $GRASS_BIN/grass$VERSION.bat
+unix2dos $GRASS_BIN/grass$VERSION.bat
