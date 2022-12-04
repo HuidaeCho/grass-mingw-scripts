a2948696b675af1f0af418ff75a1bd38501963d3
Huidae Cho <grass4u@gmail.com>
2022-12-03 18:32:00 -0700

    Add cairo

--- configure.sh
+++ configure.sh
@@ -10,11 +10,12 @@ OSGEO4W_ROOT_MSYS=$OSGEO4W_ROOT \
 ./configure \
 --host=$MINGW_CHOST \
 --with-includes=$OSGEO4W_ROOT/include \
---with-libs="$OSGEO4W_ROOT/bin $OSGEO4W_ROOT/lib" \
+--with-libs=$OSGEO4W_ROOT/lib \
 --with-nls \
 --with-freetype-includes=$OSGEO4W_ROOT/include/freetype2 \
 --with-bzlib \
 --with-netcdf \
+--with-cairo-ldflags=-lcairo \
 --with-libpng=$GRASS_SRC/mswindows/osgeo4w/libpng-config \
 --with-geos=$GRASS_SRC/mswindows/osgeo4w/geos-config \
 --with-gdal=$GRASS_SRC/mswindows/osgeo4w/gdal-config \
