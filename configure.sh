de0b247d11a9db3219e3915a5367bba7e9b986e5
Huidae Cho <grass4u@gmail.com>
2022-12-03 18:18:54 -0700

    Have to use full paths to *-config

--- configure.sh
+++ configure.sh
@@ -10,14 +10,14 @@ OSGEO4W_ROOT_MSYS=$OSGEO4W_ROOT \
 ./configure \
 --host=$MINGW_CHOST \
 --with-includes=$OSGEO4W_ROOT/include \
---with-libs="$OSGEO4W_ROOT/lib $OSGEO4W_ROOT/bin" \
+--with-libs="$OSGEO4W_ROOT/bin $OSGEO4W_ROOT/lib" \
 --with-nls \
 --with-freetype-includes=$OSGEO4W_ROOT/include/freetype2 \
 --with-bzlib \
---with-netcdf=nc-config \
---with-libpng=mswindows/osgeo4w/libpng-config \
---with-geos=mswindows/osgeo4w/geos-config \
---with-gdal=mswindows/osgeo4w/gdal-config \
---with-liblas=mswindows/osgeo4w/liblas-config \
+--with-netcdf \
+--with-libpng=$GRASS_SRC/mswindows/osgeo4w/libpng-config \
+--with-geos=$GRASS_SRC/mswindows/osgeo4w/geos-config \
+--with-gdal=$GRASS_SRC/mswindows/osgeo4w/gdal-config \
+--with-liblas=$GRASS_SRC/mswindows/osgeo4w/liblas-config \
 --with-opengl=windows \
 > configure.log 2>&1
