bfd177ae45cee8b2e0060720a0620916bb51992a
Huidae Cho <grass4u@gmail.com>
2022-12-03 21:33:00 -0700

    mkbusybat.sh updated

--- mkbusybats.sh
+++ mkbusybats.sh
@@ -4,13 +4,24 @@
 
 set -e
 . ${GRASSMINGWRC-~/.grassmingwrc}
-cd $GRASS_MINGW_SCRIPTS
+cd $GRASS_SRC
 
-grass_path=$OSGEO4W_ROOT/opt/grass
+# check architecture
+case "$MSYSTEM_CARCH" in
+x86_64)
+	arch=x86_64-w64-mingw32
+	;;
+i686)
+	arch=i686-w64-mingw32
+	;;
+*)
+	echo "$MSYSTEM_CARCH: Unsupported architecture"
+	exit 1
+esac
+
 version=`sed -n '/^INST_DIR[ \t]*=/{s/^.*grass//; p}' $GRASS_SRC/include/Make/Platform.make`
 
-for i in env sh; do
-	unix2dos -n $i.bat $grass_path/etc/$i.bat
-done
+sed -e 's/^\(set GISBASE=\).*/\1'$dist_esc'/' \
+    $GRASS_MINGW_SCRIPTS/env.bat | unix2dos > dist.$arch/etc/env.bat
 
-sed 's/@POSTFIX@/'$version'/g' grass.bat | unix2dos > $grass_path/grass.bat
+unix2dos -n $GRASS_MINGW_SCRIPTS/sh.bat dist.$arch/etc/sh.bat
