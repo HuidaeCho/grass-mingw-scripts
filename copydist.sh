b53e782b3245a118e6329e17a30c3b4ceb6e912d
Huidae Cho <grass4u@gmail.com>
2022-12-03 21:39:12 -0700

    split package.sh into copydist.sh and package.sh

---
+++ copydist.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# This script copies an already built distribution to OSGeo4W\opt\grass.
+
+set -e
+. ${GRASSMINGWRC-~/.grassmingwrc}
+cd $GRASS_SRC
+
+# check architecture
+case "$MSYSTEM_CARCH" in
+x86_64)
+	arch=x86_64-w64-mingw32
+	;;
+i686)
+	arch=i686-w64-mingw32
+	;;
+*)
+	echo "$MSYSTEM_CARCH: Unsupported architecture"
+	exit 1
+esac
+
+opt_path=$OSGEO4W_ROOT/opt
+grass_path=$opt_path/grass
+
+# copy MinGW libraries
+test -d $grass_path && rm -rf $grass_path
+test -d $opt_path || mkdir -p $opt_path
+cp -a dist.$arch $grass_path
+
+# update batch files
+sed -e 's/^\(set GISBASE=\).*/\1%OSGEO4W_ROOT%\\opt\\grass/' \
+    dist.$arch/etc/env.bat > $grass_path/etc/env.bat
+
+sed -e 's/^\(call "\).*\(\\o4w_env\.bat"\)$/\1%~dp0\\..\\..\\bin\2/' \
+    -e 's/^\(call "\).*\(\\env\.bat"\)$/\1%OSGEO4W_ROOT%\\opt\\grass\2/' \
+    -e 's/^\("%GRASS_PYTHON%" "\).*\(\\etc\\grass[0-9]*\.py".*\)$/\1%GISBASE%\2/g' \
+    bin.$arch/grass.bat > $grass_path/grass.bat
--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# This script packages already built GRASS GIS as a standalone ZIP file that
-# can be extracted to C:\.
+# This script packages an already copied distribution as a standalone ZIP
+# file that can be extracted to C:\.
 
 set -e
 . ${GRASSMINGWRC-~/.grassmingwrc}
@@ -21,24 +21,8 @@ i686)
 	exit 1
 esac
 
-opt_path=$OSGEO4W_ROOT/opt
-grass_path=$opt_path/grass
 version=`sed -n '/^INST_DIR[ \t]*=/{s/^.*grass//; p}' include/Make/Platform.make`
 date=`date +%Y%m%d`
-
-# copy MinGW libraries
-test -d $grass_path && rm -rf $grass_path
-test -d $opt_path || mkdir -p $opt_path
-cp -a dist.$arch $grass_path
-
-# update batch files
-sed -e 's/^\(set GISBASE=\).*/\1%OSGEO4W_ROOT%\\opt\\grass/' \
-    dist.$arch/etc/env.bat > $grass_path/etc/env.bat
-
-sed -e 's/^\(call "\).*\(\\o4w_env\.bat"\)$/\1%~dp0\\..\\..\\bin\2/' \
-    -e 's/^\(call "\).*\(\\env\.bat"\)$/\1%OSGEO4W_ROOT%\\opt\\grass\2/' \
-    -e 's/^\("%GRASS_PYTHON%" "\).*\(\\etc\\grass[0-9]*\.py".*\)$/\1%GISBASE%\2/g' \
-    bin.$arch/grass.bat > $grass_path/grass.bat
 
 # package
 grass_zip=$GRASS_SRC/grass$version-$arch-osgeo4w$bit-$date.zip
